2406ff4bd4dd472828ccc50536bb541e01fcf7df
Allan James Lapid <ajLapid718@gmail.com>
2018-04-19 11:37:44 -0400

    Learn nunjucks

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
 
+let locals = {
+  title: "An Example",
+  people: [
+    {name: "Gandalf"},
+    {name: "Frodo"},
+    {name: "Hermione"}
+  ]
+};
+
+nunjucks.configure("views", {noCache: true});
+nunjucks.render("index.html", locals, function(err, output) {
+  console.log(output);
+})
+
 app.use('/', (req, res, next) => {
   console.log(req.method, req.url);
   console.log(req);
   next();
 })
-
 
 app.get('/', (req, res)=>{
   res.send('hi')
